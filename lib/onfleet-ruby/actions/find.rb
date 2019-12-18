43eea4705e1fe9cc8279695e036874133c519742
Chris Anderson <christopher.anderson@gmail.com>
2019-12-18 10:50:26 -0500

    Gem was using incorrect encoding for URLs

--- lib/onfleet-ruby/actions/find.rb
+++ lib/onfleet-ruby/actions/find.rb
@@ -3,7 +3,7 @@ module Onfleet
     module Find
       module ClassMethods
         def find(field, search_term)
-          encoded_term = URI.encode_www_form_component(search_term)
+          encoded_term = URI.escape(search_term)
           url = "#{api_url}/#{field}/#{encoded_term}"
 
           response = Onfleet.request(url, :get)
